63274be3c03d3cb5219e847e45c13e23bd213e85
Bahaa Hashim <bahaahashim96@gmail.com>
2021-01-30 22:10:54 +0200

    all together

--- week1/React/prop-it-up/src/App.js
+++ week1/React/prop-it-up/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <Props lastname="Doe" firstname="Jane" age={45} haircolor="Black"/>
-      <Props lastname="Doe" firstname="Jane" age={45} haircolor="Black"/>
-      <Props lastname="Doe" firstname="Jane" age={45} haircolor="Black"/>
+      <Props lastname="Doe" firstname="Jane" age={30} haircolor="Black"/>
+      <Props lastname="Doe" firstname="Jane" age={24} haircolor="Black"/>
     </div>
   );
 }
--- week1/React/prop-it-up/src/components/Props.js
+++ week1/React/prop-it-up/src/components/Props.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
 class Props extends Component {
+    constructor(props){
+        super(props);
+            this.state={
+                age : this.props.age,
+            }
+    }
+    clickHandelr=()=> {
+        this.setState({
+            age: this.state.age +1
+        });
+    }
     render() {
         return (
             <div>
                 <h1>{this.props.lastname} , {this.props.firstname}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair color : {this.props.haircolor}</p>
+                <button onClick={this.clickHandelr}>Birthday button for {this.props.firstname}{this.props.lastname}</button>
                 
             </div>
         )
